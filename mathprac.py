15ed605361eb03c128e4b3e572f2a52de5d525a2
anunansh6 <anunansh6@gmail.com>
2025-10-28 11:42:04 +0530

    operation numberic

---
+++ mathprac.py
@@ -0,0 +1,28 @@
+ver1 = 35
+ver2 = 3
+sub =ver1*ver2 
+print(sub) 
+
+sub =ver1-ver2 
+print(sub) 
+
+
+sub =ver1+ver2 
+print(sub) 
+
+
+sub =ver1/ver2 
+print(sub) 
+
+sub =ver1//ver2 
+print(sub) 
+
+
+sub =ver1%ver2 
+print(sub) 
+
+ 
+
+
+
+
