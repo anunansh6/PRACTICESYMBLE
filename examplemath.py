e8d801871512d1e95ea34213a3879af715aa0d83
anunansh6 <anunansh6@gmail.com>
2025-11-05 19:55:01 +0530

    Add files via upload

---
+++ examplemath.py
@@ -0,0 +1,28 @@
+"""t=int(input("Total rupees"))
+a=int(input("Buy milk:"))
+b=int(input("Buy sugar:"))
+c=int(input("Buy tea:"))
+ts=(a+b+c)
+print(ts)
+print(t-ts)"""
+
+
+"""t=int(input("Total rupees"))
+a=int(input("Buy milk:"))
+b=int(input("Buy sugar:"))
+c=int(input("Buy tea:"))
+ts=((a*25)+(b*50)+(c*20))
+print(ts)
+print(t-ts)"""
+
+#discount
+
+
+a=int(input("Enter Laptop price:"))
+b=(a*20)
+print(b)
+d=((b*15)/100)
+print("Gross profit:")
+print(d)
+print("Final amount:")
+print(b-d)
