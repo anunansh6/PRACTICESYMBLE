f66d27a3401fabdf3adbc04ecc23475f86ec8009
anunansh6 <anunansh6@gmail.com>
2025-11-20 15:33:57 +0530

    some example

---
+++ oddeven.py
@@ -0,0 +1,50 @@
+# a=int(input("Enter num:"))
+# print(f"")
+# if a%2==0:
+#  	print(" number sum")
+
+# else:
+#   print("Number visham")
+# print("Program End")
+
+# a=int(input("Enter num:"))
+# print(f"")
+# if a%5==0:
+#  	print("yes divisibel by 5")
+
+# else:
+#   print("No divisibel by 5")
+# print("Program End")
+
+# a=int(input("Enter num:"))
+# print(f"")
+# if a>50 and a<70:
+#  	print("yes")
+
+# else:
+#   print("no")
+# print("Program End")
+
+
+# a=int(input("Enter year :"))
+# if a%4==0:
+#  	print("its leap year")
+
+# else:
+#   print("its not leap year")s
+# print("Program End")
+
+
+# a=int(input("Enter num 1:"))
+# b=int(input("Enter num 2:"))
+# if a>b: 
+#  print(f"bigger number is{a}")
+# else:
+#  print(f"bigger number is{b}")
+
+a=int(input("Enter num 1:"))
+b=int(input("Enter num 2:"))
+if a<b: 
+ print(f"smaller number is{a}")
+else:
+ print(f"smaller number is{b}")
