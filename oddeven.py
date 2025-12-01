6301080ea6e5836b117979fb5bad2be0b827b76d
anunansh6 <anunansh6@gmail.com>
2025-12-01 11:47:11 +0530

    question

--- oddeven.py
+++ oddeven.py
@@ -42,9 +42,140 @@
 # else:
 #  print(f"bigger number is{b}")
 
-a=int(input("Enter num 1:"))
-b=int(input("Enter num 2:"))
-if a<b: 
- print(f"smaller number is{a}")
-else:
- print(f"smaller number is{b}")
+# a=int(input("Enter num 1:"))
+# b=int(input("Enter num 2:"))
+# if a<b: 
+#  print(f"smaller number is{a}")
+# else:
+#  print(f"smaller number is{b}")
+
+
+# a=int(input("enter num 1:"))
+# b=int(input("enter num 2:"))
+# c=int(input("enter num 3:"))
+# if a<b>c:
+#     print()
+# else:    
+#     print()
+
+# a=input("enter corrector 1:")
+# b=input("enter correcctor 2:")
+# if a<e:
+#    print("smaller corrector a")
+# else:
+#    print("bigger corrector b")
+
+# a=int(input("enter num 1:"))
+# if a%3==0  and (a%5==0):
+#    print("divded by both")
+# elif a%3==0:
+#    print("divded by 3")
+# elif a%5==0:
+#    print("diveded by 5")   
+# else:
+#    print("no divided")   
+
+
+# a=int(input("First number  is :"))
+# b=int(input("Second number  is :"))
+# c=int(input("Third number is :"))
+# if a>=b and a>=c:
+#  print("large number is a")
+# elif b>=a and b>=c: 
+#  print("large number is b") 
+# else :
+#  print(" large number is c ")
+
+
+
+
+# h=input("enter input:")
+# if h=="A" or h=="E" or h=="I"or h=="O"or h=="U": 
+#  print(" upper vyanjan")
+# elif h=="a" or h=="e" or h=="i"or h=="o"or h=="u":
+#  print("small vyanjan")  
+# else :
+#  print("swar")
+
+# h=input("enter first word:")
+# if h>="a" and h<="z":
+#     print("small letter")
+# elif h>="A" and h<="Z":
+#     print("capital letter")   
+# else :
+#     print("NONE OF LETTER")       
+
+
+# x=int(input("First number is:"))
+# y=int(input("Second number is :"))
+# z=input("operater :")
+# if z=="+":
+#     print(x+y)
+# elif z=="-":
+#     print(x-y)
+# elif z=="*":
+#     print(x*y)
+# elif z=="%":
+#     print(x%y)
+# elif z=="//":
+#     print(x//y)
+# elif z=="/":
+#     print(x/y)
+#  elif z=="**":
+#     print(x**y)
+# else :
+#     print("invalid opertor") 
+
+# x=int(input("Enter date:"))
+# if x==1:
+#     print("sunday")                     
+# elif x==2:
+#     print("monday")
+# elif x==3:
+#     print("Tuesday")
+# elif x==4:
+#     print("wednesday")
+# elif x==5:
+#     print("Thursday")
+# elif x==6:
+#     print("Friday")
+# elif x==7:
+#     print("Saturday")
+# else :
+#     print("ENVALID ERORRR...")
+
+
+x=int(input("Enter Date :"))
+match x:
+
+    case 1:    
+        print("January")    
+    case 2:    
+        print("February")    
+    case 3:    
+        print("March")    
+    case 4:    
+        print("Aprail")    
+    case 5:    
+        print("May")    
+    case 6:    
+        print("June")    
+    case 7:    
+        print("July")    
+    case 8:    
+        print("August")    
+    case 9:    
+        print("September")    
+    case 10:    
+        print("October")    
+    case 11:    
+        print("Nomveber")    
+    case 12:    
+        print("December")
+    case _ :
+        print("ENVALID ERROR .....")        
+
+
+
+
+   
